83ee5da82cca87adbcbab61dd54991d60fd4bcc7
Nikolay Tabelko <pokerface-88@mail.ru>
2018-09-27 18:20:41 +0400

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+function Footer(){
+    return(
+        <div>
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="footer-item">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, nesciunt nulla illum suscipit, animi, accusamus iure magni illo ad atque similique rem incidunt eos modi harum cupiditate expedita odit asperiores!</div>
+                </div>
+                <div className="col-md-4">
+                    <div className="footer-item">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, nesciunt nulla illum suscipit, animi, accusamus iure magni illo ad atque similique rem incidunt eos modi harum cupiditate expedita odit asperiores!</div>
+                </div>
+                <div className="col-md-4">
+                    <div className="footer-item">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, nesciunt nulla illum suscipit, animi, accusamus iure magni illo ad atque similique rem incidunt eos modi harum cupiditate expedita odit asperiores!</div>
+                </div>
+            </div>
+
+
+        </div>
+
+ )
+
+}
+export default Footer 
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {render} from 'react-dom'
+import './style.css' 
+import App from './components/app'
+import Header from './components/header'
+import Body from './components/body'
+import Footer from './components/footer'
+
+render(<App />, document.getElementById('root'))
