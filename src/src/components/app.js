04e0a616ae3b0b8188daf6ec9df1f6db6b74e5de
Nikolay Tabelko <pokerface-88@mail.ru>
2018-10-01 19:23:53 +0400

    last changes

---
+++ src/src/components/app.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from './header'
+import Body from './body'
+import Footer from './footer'
+
+function App(){
+    return (
+        <div>
+            <h1>App name</h1>
+            <Header />
+            <hr />
+            <Body />
+            <hr />
+            <Footer />
+            
+
+        </div>
+    )
+}
+
+export default App
---
+++ src/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {render} from 'react-dom'
+import './style.css' 
+import App from './components/app'
+import Header from './components/header'
+import Body from './components/body'
+import Footer from './components/footer'
+import List from './components/menu'
+
+render(<App />, document.getElementById('root'))
